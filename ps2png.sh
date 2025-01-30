249b1bfbf329ee0a943ec2a9169889eccd2e9b3f
Paul Gazzillo <paul@pgazz.com>
2025-01-30 10:10:36 -0500

    Add script to convert ps to pdf using ghostscript

---
+++ ps2png.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "USAGE: ${0} file.ps [file.png]"
+	exit 1
+fi
+
+psfile="${1}"
+stem="${psfile%.ps}"
+pdffile="${stem}.pdf"
+pngfile="${stem}.png"
+
+if [ ! -f "${psfile}" ]; then
+	echo "${psfile} does not exist"
+	exit 1
+fi
+
+ps2pdf "${psfile}" "${pdffile}" 
+pdfcrop "${pdffile}" "${pdffile}"
+gs -dSAFER -dBATCH -dNOPAUSE -dEPSCrop -r600 -sDEVICE=pngalpha -sOutputFile="${pngfile}" "${pdffile}"
